67c76c5068b1ad015f0fba77eb634d7ab3a6ee52
Carissa Lauritsen <carissakoehn@gmail.com>
2022-01-28 14:47:38 -0600

    Updates to DB & User Model/routes.

--- what-the-food/models/user.js
+++ what-the-food/models/user.js
@@ -4,16 +4,17 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
   }
   User.init({
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
     firstName: {
       type: DataTypes.STRING,
       allowNull: false
--- what-the-food/routes/users.js
+++ what-the-food/routes/users.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var router = express.Router();
+var authService = require('../services/auth');
+const { User } = require('../models');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Everything is ready to go!');
 });
+
+/* GET /:id get individual user */
+
+
+/* POST Create new user */
+
+// router.post('/', function(req, res, next) {
+//   User.findOrCreate({
+//         firstName: req.body.firstName,
+//         lastName: req.body.lastName,
+//         username: req.body.username,
+//         password: req.body.password
+//     })
+//     .then(function(result, created) {
+//       if (created) {
+//         res.send('User successfully created');
+//       } else {
+//         res.send('This user already exists');
+//       }
+//     });
+// });
+
+router.post('/signup', function (req, res, next) {
+  User
+    .findOrCreate({
+      where: {
+        username: req.body.username
+      },
+      defaults: {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        username: req.body.username,
+        password: authService.hashPassword(req.body.password) //<--- Change to this code here
+      }
+    })
+    .spread(function (result, created) {
+      if (created) {
+        res.send('User successfully created');
+      } else {
+        res.send('This user already exists');
+      }
+    });
+});
+
+
+/* PUT Update user */
+
+/* DELETE delete user */
+
 
 module.exports = router;
